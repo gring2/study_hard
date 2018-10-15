08918c409e26db72b5aa004fa7425c9e46c708c3
gring2 <pjw6454@hotmail.com>
2018-10-16 00:03:44 +0900

    insta_clone

---
+++ insta_clone/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+       <Layout>
+        <Header style="padding: 0"><HeaderNav/></Header>
+        <Content>
+    <div>
+      <p>
+        If iView is successfully added to this project, you'll see an
+        <code v-text="'<Button>'"></code>
+        below
+      </p>
+      <Button type="primary">Button</Button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+        </Content>
+        <Footer>Footer</Footer>
+    </Layout>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HeaderNav from '@/components/HeaderNav'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    HeaderNav
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ insta_clone/src/components/HeaderNav.vue
@@ -0,0 +1,61 @@
+<template>
+  <Row style="height: 100%">
+    <Col span=4 style="height: 100%">
+        <img src="../assets/logo.png">
+    </Col>
+    <Col span=12>
+       <Input v-model="keyword" placeholder="Enter keyword..."  clearable/>
+    </Col>
+    <Col span=2>
+        <div>&nbsp;</div>
+    </Col>
+
+
+    <Col span=2>
+      <Icon type="ios-compass-outline" size="40" color="#fff"/>
+    </Col>
+    <Col span=2>
+
+      <Icon type="md-heart-outline" size="40" color="#fff"/>
+    </Col>
+    <Col span=2>
+
+      <Icon type="ios-contact-outline" size="40" color="#fff"/>
+    </Col>
+
+  </Row>
+</template>
+
+<script>
+export default {
+  name: 'HeaderNav',
+  data () {
+    return {
+      keyword: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+img{
+  height: 100%;
+  width: 50%
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
